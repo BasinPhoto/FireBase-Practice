542681e963a84fb8e9f9228a046472cd3b29e34d
Serg Basin <shoom.pozitiv@gmail.com>
2021-03-25 20:06:31 +0200

    Initial Commit

---
+++ FireBase Practice/FireBase_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBase_PracticeApp.swift
+//  FireBase Practice
+//
+//  Created by Sergey Basin on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct FireBase_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
